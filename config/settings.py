28cd7620fc8e356046a49768d34fca081a552be2
Achifa <akpulukelvin@gmail.com>
2023-02-28 19:11:10 +0100

    updated allowed hosts 2

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["onrender.com, .onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
